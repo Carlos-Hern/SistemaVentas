f2e0c68706590a4df719ff92b58d8f170711577c
Carlos-Hern <chernandezh18@miumg.edu.gt>
2022-10-20 09:01:49 -0600

    Se agrega el Proyecto

---
+++ src/modelo/EntidadVendedor.java
@@ -0,0 +1,67 @@
+
+package modelo;
+
+public class EntidadVendedor {
+    int id;
+    String dpi;
+    String nom;
+    String tel;
+    String user;
+
+    public EntidadVendedor() {
+    }
+
+    public EntidadVendedor(int id, String dpi, String nom, String tel, String user) {
+        this.id = id;
+        this.dpi = dpi;
+        this.nom = nom;
+        this.tel = tel;
+        this.user = user;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDpi() {
+        return dpi;
+    }
+
+    public void setDpi(String dpi) {
+        this.dpi = dpi;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    void setEstado(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
---
+++ src/modelo/VendedorDAO.java
@@ -0,0 +1,41 @@
+
+package modelo;
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class VendedorDAO {
+    PreparedStatement ps;
+    ResultSet rs;
+    
+    Conexion con = new Conexion();
+    Connection acceso;
+    
+    public EntidadVendedor ValidadVendedor(String dpi, String user){
+         EntidadVendedor ev = new EntidadVendedor();
+        String sql = "select * from vendedor where Dpi like? and User like?";
+        
+        try {
+          acceso=con.Conectar();
+          ps= acceso.prepareStatement(sql);
+          ps.setString(1, dpi);
+          ps.setString(2, user);
+          rs=ps.executeQuery();
+          while (rs.next()){
+              ev.setId(rs.getInt(1));
+              ev.setDpi(rs.getString(2));
+              ev.setNom(rs.getString(3));
+              ev.setTel(rs.getString(4));
+              ev.setUser(rs.getString(5));
+          }
+          
+        } catch (Exception e){
+            
+        }
+        
+        return ev;
+        
+    }
+}
